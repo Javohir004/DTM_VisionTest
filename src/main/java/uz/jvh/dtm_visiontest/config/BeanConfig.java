d80ef70879c838fabf4e0f78f6f24fdb2263238d
Javohir004 <javohirsharifjonov795@gmail.com>
2025-01-17 16:03:32 +0500

    daffuya ish qildim 1

---
+++ src/main/java/uz/jvh/dtm_visiontest/config/BeanConfig.java
@@ -0,0 +1,18 @@
+package uz.jvh.dtm_visiontest.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true);
+        return modelMapper;
+    }
+}
